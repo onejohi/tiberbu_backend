e4fc3ab88cf1d54689833abc5fd5c17a93570bcb
onejohi <tonnie254@live.com>
2025-04-11 23:12:23 +0300

    - add routes

--- app/main.py
+++ app/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 from app.routes import auth
 from app.routes import doctor
 from app.routes import patient
+from app.routes import appointment
+from app.routes import medical_records
+from app.routes import availability
 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(doctor.router)
+app.include_router(patient.router)
+app.include_router(appointment.router)
+app.include_router(medical_records.router)
+app.include_router(availability.router)
 
 @app.get('/')
 def read_root():
